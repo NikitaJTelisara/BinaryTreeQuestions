a00585551d0e11cadf2a65cb8eadbfd9b0c69baa
NikitaJTelisara <nikitatelisara@gmail.com>
2017-06-19 16:58:50 -0500

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+
 public class TreeNode {
     TreeNode left;
     TreeNode right;
     int data;
+    ArrayList<TreeNode> children = new ArrayList<TreeNode>();
+    int numberOfChildren = 0;
 
     public TreeNode(int data){
         this.data = data;
+    }
+
+    public void addChildren(TreeNode n){
+         this.children.add(n);
+         numberOfChildren++;
+    }
+
+    public void removeChildren(TreeNode n){
+        this.children.remove(n);
+        numberOfChildren--;
     }
 }
